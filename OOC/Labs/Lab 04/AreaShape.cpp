c5935f25b4d4adeac02fe6e87f6f05279f61b165
IT21277436 <99390282+IT21277436@users.noreply.github.com>
2022-03-07 11:36:44 +0530

    Create AreaShape.cpp

---
+++ OOC/Labs/Lab 04/AreaShape.cpp
@@ -0,0 +1,16 @@
+#include "ShapeArea.h"
+
+float areaCircle(float r)
+{
+	return (22 / 7.0*r*r);
+}
+
+float areaRectangle(float l, float w)
+{
+	return (l*w);
+}
+
+float areaSquare(float l)
+{
+	return (l*l);
+}
