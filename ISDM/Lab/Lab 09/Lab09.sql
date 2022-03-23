69fb7872d532f6abaedcc31a9cff2732eba4a625
IT21277436 <99390282+IT21277436@users.noreply.github.com>
2022-03-23 15:20:41 +0530

    Create Lab09.sql

---
+++ ISDM/Lab/Lab 09/Lab09.sql
@@ -0,0 +1,12 @@
+INSERT INTO Course VALUES ('IT', 'Information Technology', 'The programme is designed for technically focused students who capabilities in programming', 175000),
+('SE', 'Software Engineering', 'Software engineering is the discipline of designing, creating and maintaining', 185000),
+('CSNE', 'Computer Systems And Network Engineering', 'The programme aims to provide students with the knowledge, skills, planning, and designing', 155000),
+('DS', 'Data Scinece', 'The meticulous curriculum focuses on the fundamentals of computer science, statistics, and applied mathematics', 170000);
+
+
+
+
+
+UPDATE Student SET Address='No15,Gamunupura,New Kandy Rd, Malabe' WHERE SID='IT18234568';
+
+DELETE FROM Module WHERE Mcode='SE3050';
